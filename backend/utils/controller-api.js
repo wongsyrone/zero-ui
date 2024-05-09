5be2427f29218672540e902a2a70cbfc2aeeffb0
Syrone Wong <wong.syrone@gmail.com>
2024-05-09 08:42:10 +0800

    feat: add support to default authtoken location of major platforms
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- backend/utils/controller-api.js
+++ backend/utils/controller-api.js
@@ -9,6 +9,16 @@ if (process.env.ZU_CONTROLLER_TOKEN) {
   token = process.env.ZU_CONTROLLER_TOKEN;
 } else if (os.platform() === "linux") {
   token = fs.readFileSync("/var/lib/zerotier-one/authtoken.secret", "utf8");
+} else if (process.platform === "win32") {
+  token = fs.readFileSync(
+    "C:\\ProgramData\\ZeroTier\\One\\authtoken.secret",
+    "utf8"
+  );
+} else if (process.platform === "darwin") {
+  token = fs.readFileSync(
+    "/Library/Application Support/ZeroTier/One/authtoken.secret",
+    "utf8"
+  );
 } else {
   throw new Error("Please provide ZU_CONTROLLER_TOKEN in environment");
 }
